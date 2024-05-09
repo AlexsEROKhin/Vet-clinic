9811791d498c17a1a03965013fa917ecb1646619
AleksEROKhin <erohin372@gmail.com>
2024-05-10 01:01:32 +0200

    REQ-02: Email validation

--- src/main/java/com/magicvet/Main.java
+++ src/main/java/com/magicvet/Main.java
@@ -10,7 +10,11 @@ public class Main {
 
     static Scanner SCANNER = new Scanner(System.in);
 
+    private static final String NAME_PATTERN = "^[a-zA-Z-' ]{3,}$";
+
     static String EMAIL_PATTERN = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+
+
     public static void main(String[] args) {
         run();
     }
@@ -25,7 +29,6 @@ public class Main {
         for(int i = 0;i < 3; i++){
             System.out.print("Password:");
             String input =SCANNER.nextLine();
-
             if(PASSWORD.equals(input)){
                 accepted = true;
                 break;
@@ -41,21 +44,38 @@ public class Main {
         System.out.print("Email: ");
         String email = SCANNER.nextLine();
         if(isEmailValid(email)){
-            Client client = buildClient(email);
-            System.out.println("New client: " + client.firstName + " " + client.lastName + " (" + client.email + ")");
+            System.out.print("First name: ");
+            String firstName = SCANNER.nextLine();
+            System.out.print("Last name: ");
+            String lastName = SCANNER.nextLine();
+            if(isNameValid(firstName) && isNameValid(lastName)){
+                Client client = buildClient(email,firstName,lastName);
+                System.out.println("New client: " + client.firstName + " " + client.lastName + " (" + client.email + ")");
+            }else{
+                System.out.println("Provided first name or last name is invalid");
+            }
+
         }else{
             System.out.println("Provided email is invalid");
         }
+
+
+
     }
 
-    static Client buildClient(String email) {
+    private static boolean isNameValid(String firstName) {
+        Pattern pattern = Pattern.compile(NAME_PATTERN);
+        Matcher matcher = pattern.matcher(firstName);
+        return matcher.matches();
+    }
+
+
+    static Client buildClient(String email,String firstName,String lastName) {
         Client client = new Client();
         client.email = email;
-        System.out.print("First name: ");
-        client.firstName = SCANNER.nextLine();
+        client.firstName = firstName;
+        client.lastName = lastName;
 
-        System.out.print("Last name: ");
-        client.lastName = SCANNER.nextLine();
 
         return client;
     }
