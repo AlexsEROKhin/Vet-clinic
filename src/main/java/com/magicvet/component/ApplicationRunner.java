61741af9f3e920d2ee6e5415e593326e1dd454d6
AleksEROKhin <erohin372@gmail.com>
2024-05-10 02:17:54 +0200

    REQ-03: Add new pet

--- src/main/java/com/magicvet/component/ApplicationRunner.java
+++ src/main/java/com/magicvet/component/ApplicationRunner.java
@@ -1,23 +1,31 @@
 package com.magicvet.component;
 
+import com.magicvet.Main;
 import com.magicvet.model.Client;
 import com.magicvet.model.Pet;
 import com.magicvet.service.ClientService;
 import com.magicvet.service.PetService;
 
 public class ApplicationRunner {
-    private ClientService clientService = new ClientService();
-    private PetService petService = new PetService();
+    private final ClientService clientService = new ClientService();
+    private final PetService petService = new PetService();
     public void run() {
         if(Authenticator.auth()){
             Client client = clientService.registerNewClient();
 
             if(client != null){
-                System.out.println("Adding a new pet.");
-                Pet pet = petService.registerNewPet();
-                client.setPet(pet);
-                pet.setOwnerName(client.getFirstName() + " " + client.getLastName());
-                System.out.println("Pet has been added.");
+                System.out.print("Would you like to register your pet? (yes/no): ");
+                String choice = Main.SCANNER.nextLine();
+                if (choice.equalsIgnoreCase("yes")) {
+                    System.out.println("Adding a new pet.");
+                    Pet pet = petService.registerNewPet();
+                    client.setPet(pet);
+                    pet.setOwnerName(client.getFirstName() + " " + client.getLastName());
+                    System.out.println("Pet has been added.");
+                }else{
+                    System.out.println("Pet has not been added.");
+                }
+
             }
         }
     }
