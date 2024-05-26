54c095b264530b7be32c39af225968ec8eb9c697
AleksEROKhin <erohin372@gmail.com>
2024-05-26 17:24:55 +0200

    REQ-07: Use enum for dog size

--- src/main/java/com/magicvet/model/Dog.java
+++ src/main/java/com/magicvet/model/Dog.java
@@ -4,15 +4,10 @@ import java.util.Objects;
 
 public class Dog extends Pet{
 
-    public static final String xS = "xS";
-    public static final String S = "S";
-    public static final String M = "M";
-    public static final String L = "L";
-    public static final String xxL = "xxL";
-    private String size;
+    private Size size;
 
     public Dog(){ }
-    public Dog(String size){
+    public Dog(Size size){
         this.size = size;
     }
 
@@ -40,11 +35,30 @@ public class Dog extends Pet{
         return Objects.hash(size);
     }
 
-    public String getSize() {
+    public Size getSize() {
         return size;
     }
 
-    public void setSize(String size) {
+    public void setSize(Size size) {
         this.size = size;
     }
+
+    public enum Size {
+        XS(1),
+        S(2),
+        M(3),
+        L(4),
+        XL(5),
+        UNKNOWN(0);
+        private final int value;
+
+        Size(int value){
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
 }
