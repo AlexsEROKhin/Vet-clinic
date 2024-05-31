8d613dfaad8d0249cd50225eb9ac1c0aa2bac94a
AleksEROKhin <erohin372@gmail.com>
2024-05-31 20:09:19 +0200

    REQ-08: Add registration date for Pet class

--- src/main/java/com/magicvet/Sandbox.java
+++ src/main/java/com/magicvet/Sandbox.java
@@ -1,5 +1,6 @@
 package com.magicvet;
 
+import com.magicvet.model.Cat;
 import com.magicvet.model.Dog;
 
 import java.util.Arrays;
@@ -7,7 +8,18 @@ import java.util.Comparator;
 
 public class Sandbox {
     public static void main(String[] args){
-        Dog[] dogs = {
+        System.out.println(new Cat());
+
+
+
+
+
+
+
+
+
+
+        /*Dog[] dogs = {
                 new Dog(Dog.Size.M, Dog.HealthState.CRITICAL),
                 new Dog(Dog.Size.S,Dog.HealthState.GOOD),
                 new Dog(Dog.Size.XL,Dog.HealthState.FAIR),
@@ -29,7 +41,7 @@ public class Sandbox {
         System.out.println("Sorted by health state (from worst to best):");
         for(Dog dog: dogs){
             System.out.println(dog.getSize() + ", " + dog.getHealthState());
-        }
+        }*/
 
 
     }
