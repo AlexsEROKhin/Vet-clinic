b37780e3c51803cefc0097d640c2163a1d0dde4f
AleksEROKhin <erohin372@gmail.com>
2024-06-16 16:00:02 +0200

    Bug fix: Fix IllegalArgumentException for enums

--- src/main/java/com/magicvet/model/Pet.java
+++ src/main/java/com/magicvet/model/Pet.java
@@ -6,7 +6,7 @@ import java.util.Objects;
 
 public abstract class Pet {
 
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("HH:mm dd/MM/yyyy");
+    static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("HH:mm dd/MM/yyyy");
     private String type;
     private String sex;
     private String age;
@@ -78,6 +78,10 @@ public abstract class Pet {
 
     public void setOwnerName(String ownerName) {
         this.ownerName = ownerName;
+    }
+
+    public LocalDateTime getRegistrationDate() {
+        return registrationDate;
     }
 }
 
